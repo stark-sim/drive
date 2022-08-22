7abf37560369903b9c8c41cae59f05d8fe4d19e4
shenda <shenda@hqshuke.com>
2022-08-22 17:17:15 +0800

    set Allow-Origin to request.origin

--- middleware/cors.go
+++ middleware/cors.go
@@ -13,7 +13,7 @@ func CorsMiddleware() gin.HandlerFunc {
 		if len(origin) == 0 {
 			origin = c.Request.Header.Get("Origin")
 		}
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Add("Access-Control-Allow-Headers", "Content-Type, Access-Token, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE")
