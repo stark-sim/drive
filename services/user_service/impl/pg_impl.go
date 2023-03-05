6fbf81c765377dba6807d76ce1e81d1f1732287c
枕哒 <gooda159753@163.com>
2023-03-05 11:43:27 +0800

    feat: confirm ent better than gorm

--- services/user_service/impl/pg_impl.go
+++ services/user_service/impl/pg_impl.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"drive/common"
 	"drive/ent"
+	"drive/ent/email"
+	"drive/ent/social"
 	"drive/ent/user"
 	"drive/services/user_service"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"time"
 )
@@ -60,6 +63,8 @@ func (p *pgImpl) DeleteOne(ctx context.Context, id int64) (res *ent.User, err er
 }
 
 func (p *pgImpl) GetByPhone(ctx context.Context, phone string) (res *ent.User, err error) {
+	_social, err := p.dbClient.Debug().Social.Query().Where(social.HasEmailWith(email.Name("hello"))).Count(ctx)
+	fmt.Printf("%+v\n", _social)
 	_user, err := p.dbClient.User.Query().Where(user.PhoneEQ(phone)).First(ctx)
 	if err != nil {
 		logrus.Errorf("get user by phone error: %v", err)
